45692e1751592eaf03bd6508a8fad3c93ccbc0e2
tvoyserezha <tvoyserezha@gmail.com>
2022-06-26 07:32:44 +0500

    b64_prefix

--- frontend/src/api/methods.js
+++ frontend/src/api/methods.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {getBase64} from "../helpers/b64";
+import {base64ToFile, getBase64} from "../helpers/b64";
 
 const BASE_URL = 'http://localhost:5000'
 
@@ -7,11 +7,20 @@ export const sendPhoto = async photo => {
   let response
   const fileBase64 = await getBase64(photo)
   const img = fileBase64.split(',').slice(1, fileBase64.length)
-  const data = {img: img}
+  const b64_prefix = fileBase64.split(',')[0]
+  const data = {
+    img,
+    b64_prefix,
+  }
+
   try {
     response = axios.post(`${BASE_URL}/predict`, data)
   } catch (e) {
   }
 
+  let fetchedImageBase64
   return response
+    .then(r => fetchedImageBase64 = r.data.img).then(() => {
+      return base64ToFile(fetchedImageBase64, "Image", "image/png")
+  })
 }
--- frontend/src/helpers/b64.js
+++ frontend/src/helpers/b64.js
@@ -4,3 +4,14 @@ export const getBase64 = file => new Promise((resolve, reject) => {
   reader.onload = () => resolve(reader.result);
   reader.onerror = error => reject(error);
 });
+
+export const base64ToFile = (url, filename, mimeType) => {
+  return (fetch(url)
+      .then(function (res) {
+        return res.arrayBuffer();
+      })
+      .then(function (buf) {
+        return new File([buf], filename, {type: mimeType});
+      })
+  )
+}
