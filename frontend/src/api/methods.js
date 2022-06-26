9db23be4484fd891c7dfac374ac809ba6bd47c13
tvoyserezha <tvoyserezha@gmail.com>
2022-06-26 06:02:18 +0500

    Исправил отправку base64 строки

--- frontend/src/api/methods.js
+++ frontend/src/api/methods.js
@@ -4,13 +4,14 @@ import {getBase64} from "../helpers/b64";
 const BASE_URL = 'http://localhost:5000'
 
 export const sendPhoto = async photo => {
-    let response
-    const fileBase64 = await getBase64(photo)
-    const data = {img: fileBase64}
-    try {
-        response = axios.post(`${BASE_URL}/predict`, data)
-    } catch (e) {
-    }
+  let response
+  const fileBase64 = await getBase64(photo)
+  const img = fileBase64.split(',').slice(1, fileBase64.length)
+  const data = {img: img}
+  try {
+    response = axios.post(`${BASE_URL}/predict`, data)
+  } catch (e) {
+  }
 
-    return response
+  return response
 }
